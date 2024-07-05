b3e2739a56b9a0541562c5b5d5f11dbb12abb066
David Martínez <dmmtapia.ux@gmail.com>
2024-07-05 12:04:42 -0400

    component-ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'arquetipo';
+
+  forma!:FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.crearFormulario()
+  }
+
+  crearFormulario(){
+    this.forma = this.fb.group({
+      nombre:[""],
+      apellido:[""],
+      correo:[""],
+      password1:[""],
+      password2:[""]
+    })
+  }
+
+  guardar(){
+    console.log(this.forma);
+  }
+
+  limpiar(){
+    this.forma.reset();
+  }
 }
