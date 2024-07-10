02d5bd08a55c29ce1c8c1affe03cdb5d6c03713e
David Martínez <dmmtapia.ux@gmail.com>
2024-07-10 07:28:04 -0400

    module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
+
+import { AppComponent } from './app.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
     
   ],
   providers: [],
