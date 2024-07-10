9ac73e103f0a3c05f1716349c483dead2bcb76d4
David Martínez <dmmtapia.ux@gmail.com>
2024-07-10 07:27:28 -0400

    component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UsuariosService } from './services/usuarios.service';
+
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'arquetipo';
 
-  constructor() {
-    
+  constructor(private usuariosService: UsuariosService) {
+    this.usuariosService.obtenerUsuarios().subscribe(resp => {
+      console.log(resp)
+    })
   }
 }
   
