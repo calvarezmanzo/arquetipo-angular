820a6cfd415c445a3d0f95172e8208bd1d95ddf7
Anthony Muñoz <anthonymo577@gmail.com>
2024-07-14 17:06:27 -0400

    cambios tony

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.formularioEjemplo = this.formBuilder.group({
-      Nombre:['CARLOS',[Validators.required,Validators.minLength(20)]],  
+      Nombre:['Tony',[Validators.required,Validators.minLength(20)]],  
       Apellido:['',[Validators.required,Validators.minLength(20)]], 
       Rut:['',[Validators.required]], 
     })
